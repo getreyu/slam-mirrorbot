d79cba118eeea85a25d725759f8766a9a9f0b7d5
getreyu <76866622+getreyu@users.noreply.github.com>
2021-04-14 16:47:59 +0700

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -159,9 +159,9 @@ class MirrorListener(listeners.MirrorListeners):
                     share_url += '/'
                 if SHORTENER is not None and SHORTENER_API is not None:
                     siurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, share_url)).text
-                    buttons.buildbutton("⚡Index Link⚡", siurl)
+                    buttons.buildbutton("Index Link", siurl)
                 else:
-                    buttons.buildbutton("⚡Index Link⚡", share_url)
+                    buttons.buildbutton("Index Link", share_url)
             if BUTTON_THREE_NAME is not None and BUTTON_THREE_URL is not None:
                 buttons.buildbutton(f"{BUTTON_THREE_NAME}", f"{BUTTON_THREE_URL}")
             if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
@@ -173,7 +173,7 @@ class MirrorListener(listeners.MirrorListeners):
             else:
                 uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
             if uname is not None:
-                msg += f'\n\ncc: {uname}'
+                msg += f'\n\n🎲Di Mirror oleh: {uname}'
             try:
                 fs_utils.clean_download(download_dict[self.uid].path())
             except FileNotFoundError:
